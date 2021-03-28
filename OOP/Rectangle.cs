9e26e8c544cf1a84dccf34cf4df1f6bc8b538657
Naufal Aji Wibowo <naufal.aji@mii.co.id>
2021-03-28 16:01:23 +0700

    initial commit

---
+++ OOP/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Rectangle : Processing
+    {
+        public Rectangle(int valueA, int valueB)
+        {
+            ValueA = valueA;
+            ValueB = valueB;
+        }
+
+        public int ValueA { get; private set; }
+        public int ValueB { get; private set; }
+
+        public override double Proceed()
+        {
+            return ValueA * ValueB;
+        }
+    }
+}
